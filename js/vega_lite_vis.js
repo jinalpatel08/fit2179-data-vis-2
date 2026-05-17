692cf78caf9756041b94c30dccd8d0ffe7c9b0f2
jinalpatell <jpat0069@student.monash.edu>
2026-05-17 23:10:45 +1000

    add choropleth map chart 2

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -1,5 +1,10 @@
 var tempAnomalyChart = "https://raw.githubusercontent.com/jinalpatel08/fit2179-data-vis-2/main/js/temp_anomaly.vg.json";
+var ausMapChart = "https://raw.githubusercontent.com/jinalpatel08/fit2179-data-vis-2/main/js/aus_map.vg.json";
 
 vegaEmbed("#temp_anomaly_chart", tempAnomalyChart)
+  .then(function(result) {})
+  .catch(console.error);
+
+vegaEmbed("#aus_map_chart", ausMapChart)
   .then(function(result) {})
   .catch(console.error);
