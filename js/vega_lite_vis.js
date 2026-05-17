86dee920bcd0d278b0ef0b8987e4b92dded317ad
jinalpatell <jpat0069@student.monash.edu>
2026-05-17 22:53:46 +1000

    fix json path to absolute github raw url

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -1,4 +1,4 @@
-var tempAnomalyChart = "js/temp_anomaly.vg.json";
+var tempAnomalyChart = "https://raw.githubusercontent.com/jinalpatel08/fit2179-data-vis-2/main/js/temp_anomaly.vg.json";
 
 vegaEmbed("#temp_anomaly_chart", tempAnomalyChart)
   .then(function(result) {})
