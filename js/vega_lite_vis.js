d9c390c467aa5f1285e85f2b6e0147864fdaeff5
jinalpatell <jpat0069@student.monash.edu>
2026-05-18 23:18:09 +1000

    add state deviation bar chart

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -1,6 +1,7 @@
 var tempAnomalyChart = "https://raw.githubusercontent.com/jinalpatel08/fit2179-data-vis-2/main/js/temp_anomaly.vg.json";
 var ausMapChart = "https://raw.githubusercontent.com/jinalpatel08/fit2179-data-vis-2/main/js/aus_map.vg.json";
 var rainfallChart = "https://raw.githubusercontent.com/jinalpatel08/fit2179-data-vis-2/main/js/rainfall.vg.json";
+var stateBarChart = "https://raw.githubusercontent.com/jinalpatel08/fit2179-data-vis-2/main/js/state_bar.vg.json";
 
 
 vegaEmbed("#temp_anomaly_chart", tempAnomalyChart)
@@ -13,5 +14,9 @@ vegaEmbed("#aus_map_chart", ausMapChart)
 
 
 vegaEmbed("#rainfall_chart", rainfallChart)
+  .then(function(result) {})
+  .catch(console.error);
+
+vegaEmbed("#state_bar_chart", stateBarChart)
   .then(function(result) {})
   .catch(console.error);
