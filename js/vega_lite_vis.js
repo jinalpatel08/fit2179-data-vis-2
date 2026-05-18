578795bb35f8cff063470e456c9e6ccdcc9b9940
jinalpatell <jpat0069@student.monash.edu>
2026-05-18 22:56:50 +1000

    add rainfall area chart

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -1,10 +1,17 @@
 var tempAnomalyChart = "https://raw.githubusercontent.com/jinalpatel08/fit2179-data-vis-2/main/js/temp_anomaly.vg.json";
 var ausMapChart = "https://raw.githubusercontent.com/jinalpatel08/fit2179-data-vis-2/main/js/aus_map.vg.json";
+var rainfallChart = "https://raw.githubusercontent.com/jinalpatel08/fit2179-data-vis-2/main/js/rainfall.vg.json";
+
 
 vegaEmbed("#temp_anomaly_chart", tempAnomalyChart)
   .then(function(result) {})
   .catch(console.error);
 
 vegaEmbed("#aus_map_chart", ausMapChart)
+  .then(function(result) {})
+  .catch(console.error);
+
+
+vegaEmbed("#rainfall_chart", rainfallChart)
   .then(function(result) {})
   .catch(console.error);
