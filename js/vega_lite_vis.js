dc0e41560ddd5046af8615eede866ae122e08c4a
jinalpatell <jpat0069@student.monash.edu>
2026-05-19 14:49:53 +1000

    add melbourne water storage heatmap

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -2,7 +2,7 @@ var tempAnomalyChart = "https://raw.githubusercontent.com/jinalpatel08/fit2179-d
 var ausMapChart = "https://raw.githubusercontent.com/jinalpatel08/fit2179-data-vis-2/main/js/aus_map.vg.json";
 var rainfallChart = "https://raw.githubusercontent.com/jinalpatel08/fit2179-data-vis-2/main/js/rainfall.vg.json";
 var stateBarChart = "https://raw.githubusercontent.com/jinalpatel08/fit2179-data-vis-2/main/js/state_bar.vg.json";
-
+var waterHeatmap = "https://raw.githubusercontent.com/jinalpatel08/fit2179-data-vis-2/main/js/water_heatmap.vg.json";
 
 vegaEmbed("#temp_anomaly_chart", tempAnomalyChart)
   .then(function(result) {})
@@ -18,5 +18,10 @@ vegaEmbed("#rainfall_chart", rainfallChart)
   .catch(console.error);
 
 vegaEmbed("#state_bar_chart", stateBarChart)
+  .then(function(result) {})
+  .catch(console.error);
+
+
+vegaEmbed("#water_heatmap", waterHeatmap)
   .then(function(result) {})
   .catch(console.error);
